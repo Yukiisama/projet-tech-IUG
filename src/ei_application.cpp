65958be6d37acba6b352854685379e269167efe2
alegendre001 <alexandre.legendre@etu.u-bordeaux.fr>
2019-02-01 16:51:10 +0100

    Application

--- src/ei_application.cpp
+++ src/ei_application.cpp
@@ -2,9 +2,13 @@
 #include "ei_event.h"
 #include "ei_widget.h"
 #include "ei_application.h"
+#include "hw_interface.h"
 namespace ei {
 
-
+  uint32_t ColorToUInt(color_t color){
+    return (uint32_t)((color.alpha << 24) | (color.red << 16) |
+                  (color.green << 8)  | (color.blue << 0));
+  }
     /**
      * \brief Creates an application.
      *    <ul>
@@ -16,6 +20,13 @@ namespace ei {
      * @param main_window_size  the size of the root window of the application.
      */
     Application::Application(Size* main_window_size){
+      if(main_window_size == NULL){
+        fprintf(stderr,"Error occured for Application::Application - main_window_size is NULL\n");
+        exit(EXIT_FAILURE);
+      }
+      hw_init();
+      this->root_window = hw_create_window(main_window_size, EI_FALSE);
+      this->root_widget = Widget("Frame", NULL);
     }
 
     /**
@@ -23,6 +34,9 @@ namespace ei {
      *    (eventually calls \ref hw_quit).
      */
     Application::~Application(){
+      hw_surface_unlock(this->root_window);
+      hw_surface_free(this->root_window);
+      hw_quit();
     }
 
     /**
@@ -49,7 +63,9 @@ namespace ei {
      *    when pressing the "Escape" key).
      */
     void Application::quit_request(){
-        return;
+        if(event.param.display.closed == EI_TRUE || event.param.key.key_sym == 59){
+          Application::~Application();
+        }
     }
 
     /**
@@ -59,7 +75,7 @@ namespace ei {
      * @return      The root widget.
      */
     Frame* Application::root_widget(){
-        return NULL;
+        return this->root_widget;
     }
 
     /**
@@ -69,7 +85,7 @@ namespace ei {
      * @return      The surface of the root window.
      */
     surface_t Application::root_surface(){
-        return;
+        return this->root_window;
     }
 
     /**
@@ -81,7 +97,19 @@ namespace ei {
      *              at this location (except for the root widget).
      */
     Widget* Application::widget_pick (const Point& where){
+      if((where.x < 0 || where.x > this->root_window.size.width)
+      || (where.y < 0 || where.y > this->root_window.size.height)){
+          fprintf(stderr,"Error occured for Application::widget_pick - param where is out the root_window\n");
+          exit(EXIT_FAILURE);
+      }
+      color_t color = hw_get_pixel(this->root_window, where);
+      /*
+      if(color.red == 0 && color.green == 0 && color.blue == 0 && color.alpha == 0){
         return NULL;
+      }
+      */
+      uint32_t ID = ColorToUInt(color);
+      return pick(ID);
     }
 
 }
