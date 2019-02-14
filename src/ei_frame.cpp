9d436a31a35aa9137b8be8914e29c90c7e41832a
jdakata <jean-david.akata@etu.u-bordeaux.fr>
2019-02-14 18:18:47 +0100

    draw function

--- src/ei_frame.cpp
+++ src/ei_frame.cpp
@@ -38,15 +38,15 @@ namespace ei {
           fprintf(stderr,"Error occured for Frame::draw - surface is not valid\n");
           exit(EXIT_FAILURE);
         }
-        /*
-        if(pick_surface == nullptr){
+
+        /*if(pick_surface == nullptr){
           fprintf(stderr,"Error occured for Frame::draw - pick_surface is not vaild\n");
           exit(EXIT_FAILURE);
-        }
-        */
+        }*/
+
         //printf("dkqdfjoqjfqj\n");
 
-        color_t color_test={123,123,123,255};
+        //color_t color_test={123,123,123,255};
         linked_point_t list_frame;
         list_frame.push_back(screen_location.top_left);
         list_frame.push_back(Point(screen_location.top_left.x()+requested_size.width(),
@@ -56,14 +56,12 @@ namespace ei {
         list_frame.push_back(Point(screen_location.top_left.x(),
                                    screen_location.top_left.y()+requested_size.height()));
 
-        draw_polygon(surface,  list_frame,
-                     color,clipper);
+        draw_polygon(surface,list_frame,color,clipper);
+        //draw_polygon(pick_surface,list_frame,pick_color,clipper);
 
         for(std::list<Widget*>::iterator it = children.begin();it!= children.end();it++){
-            (*it)->draw(surface,NULL,clipper);
+            (*it)->draw(surface,pick_surface,clipper);
         }
-
-
     }
 
     /**
