114a4c10274c4ecbdff8442a93ed6c9ed7ca2f20
alegendre001 <alexandre.legendre@etu.u-bordeaux.fr>
2019-02-21 19:18:44 +0100

    Draw

--- src/ei_toplevel.cpp
+++ src/ei_toplevel.cpp
@@ -24,41 +24,81 @@ requested_size = new Size(320,240);
     void Toplevel::draw (surface_t surface,
                        surface_t pick_surface,
                        Rect*     clipper){
-                         /*
+
         if(surface == nullptr){
             fprintf(stderr,"Error occured for Frame::draw - surface is not valid\n");
             exit(EXIT_FAILURE);
         }
-        /*
-        Size top_size = {requested_size.width(),requested_size.height()*0.10};
-        Rect button_rect = Rect(screen_location.top_left,requested_size);
-        //color_t color_top
-        draw_button(surface,&button_rect,color,*corner_radius,clipper);
-
         //White frame
+
+        color_t color_white;
+        color_white.red = 255;
+        color_white.green = 255;
+        color_white.blue = 255;
+        color_white.alpha = 255;
+
+        color_t color_red;
+        color_red.red = 255;
+        color_red.green = 0;
+        color_red.blue = 0;
+        color_red.alpha = 255;
+
         linked_point_t list_frame;
-        Point white_rect;
-        white_rect.x() = screen_location.top_left.x();
-        white_rect.y() = screen_location.top_left.y()*0.90;
+        list_frame.push_back(screen_location.top_left);
 
-        list_frame.push_back(white_rect);
+        list_frame.push_back(Point(screen_location.top_left.x()+(requested_size)->width(),
+                                   screen_location.top_left.y()));
 
-        list_frame.push_back(Point(screen_location.top_left.x()+requested_size.width(),
-                                   white_rect.y()));
-
-        list_frame.push_back(Point(screen_location.top_left.x()+requested_size.width(),
-                                   screen_location.top_left.y()+requested_size.height()));
+        list_frame.push_back(Point(screen_location.top_left.x()+(requested_size)->width(),
+                                   screen_location.top_left.y()+(requested_size)->height()));
 
         list_frame.push_back(Point(screen_location.top_left.x(),
-                                   screen_location.top_left.y()+requested_size.height()));
+                                   screen_location.top_left.y()+(requested_size)->height()));
 
-        draw_polygon(surface,list_frame,color,clipper);
-        */
-        /*
+        draw_polygon(surface,list_frame,default_background_color,clipper);
+        draw_polygon(pick_surface,list_frame,pick_color,clipper);
+
+        float border = (float)*border_width;
+        linked_point_t list_white_frame;
+        list_white_frame.push_back(Point(screen_location.top_left.x()+border,
+                                   screen_location.top_left.y()*4));
+
+        list_white_frame.push_back(Point(screen_location.top_left.x()+(requested_size)->width()- border,
+                                   screen_location.top_left.y()*4));
+
+        list_white_frame.push_back(Point(screen_location.top_left.x()+(requested_size)->width()- border,
+                                   screen_location.top_left.y()+(requested_size)->height()- border));
+
+        list_white_frame.push_back(Point(screen_location.top_left.x()+border,
+                                   screen_location.top_left.y()+(requested_size)->height()- border));
+
+        draw_polygon(surface,list_white_frame,color_white,clipper);
+
+        linked_point_t list_bottom_right_button;
+        list_bottom_right_button.push_back(Point(screen_location.top_left.x()+(requested_size)->width(),
+                                   screen_location.top_left.y()+(requested_size)->height()));
+        list_bottom_right_button.push_back(Point((screen_location.top_left.x()+(requested_size)->width())*0.90,
+                                   screen_location.top_left.y()+(requested_size)->height()));
+        list_bottom_right_button.push_back(Point((screen_location.top_left.x()+(requested_size)->width())*0.90,
+                                   (screen_location.top_left.y()+(requested_size)->height())*0.90));
+        list_bottom_right_button.push_back(Point((screen_location.top_left.x()+(requested_size)->width()),
+                                   (screen_location.top_left.y()+(requested_size)->height())*0.90 ));
+
+        draw_polygon(surface,list_bottom_right_button,default_background_color,clipper);
+
+        if(title){
+            Point where = {(int) (screen_location.top_left.x()*1.50), (int) (screen_location.top_left.y()*1.10)};
+            draw_text(surface,&where,*title,hw_text_font_create(default_font_filename, font_default_size),&color_white);
+        }
+
+        Size button_top_left_size = {(float)(((requested_size)->width())*0.05),(float)(((requested_size)->height())*0.05)};
+        Rect button_rect = Rect(screen_location.top_left,button_top_left_size);
+        //draw_button(surface,&button_rect,color_red,default_button_corner_radius,clipper);
+
         for(std::list<Widget*>::iterator it = children.begin();it!= children.end();it++){
             (*it)->draw(surface,pick_surface,clipper);
         }
-        */
+
         return;
     }
 
