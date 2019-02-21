e49810b6ea23e5291e7623c333568234ecfb1f83
alegendre001 <alexandre.legendre@etu.u-bordeaux.fr>
2019-02-21 16:28:38 +0100

    Maj toplevel

--- src/ei_toplevel.cpp
+++ src/ei_toplevel.cpp
@@ -8,24 +8,57 @@
 
 namespace ei {
 
-    Toplevel::Toplevel(Widget *parent) : Widget("Toplevel", parent){
-      requested_size->width() = 320;
-      requested_size->height() = 240;
-      *border_width = 4;
-      *title = "Toplevel";
-      *closable = EI_TRUE;
-      *resizable = ei_axis_both;
-      min_size->width() = 160;
-      min_size->height() = 120;
-    }
+Toplevel::Toplevel(Widget *parent) : Widget("Toplevel", parent){
+requested_size = new Size(320,240);
+    border_width = new int(4);
+    title = new const char*("Toplevel");
+    closable = new bool_t(EI_TRUE);
+    resizable = new axis_set_t (ei_axis_both);
+    min_size = new Size(160,120);
+}
 
     Toplevel::~Toplevel(){
 
     }
 
-    void Widget::draw (surface_t surface,
+    void Toplevel::draw (surface_t surface,
                        surface_t pick_surface,
                        Rect*     clipper){
+                         /*
+        if(surface == nullptr){
+            fprintf(stderr,"Error occured for Frame::draw - surface is not valid\n");
+            exit(EXIT_FAILURE);
+        }
+        /*
+        Size top_size = {requested_size.width(),requested_size.height()*0.10};
+        Rect button_rect = Rect(screen_location.top_left,requested_size);
+        //color_t color_top
+        draw_button(surface,&button_rect,color,*corner_radius,clipper);
+
+        //White frame
+        linked_point_t list_frame;
+        Point white_rect;
+        white_rect.x() = screen_location.top_left.x();
+        white_rect.y() = screen_location.top_left.y()*0.90;
+
+        list_frame.push_back(white_rect);
+
+        list_frame.push_back(Point(screen_location.top_left.x()+requested_size.width(),
+                                   white_rect.y()));
+
+        list_frame.push_back(Point(screen_location.top_left.x()+requested_size.width(),
+                                   screen_location.top_left.y()+requested_size.height()));
+
+        list_frame.push_back(Point(screen_location.top_left.x(),
+                                   screen_location.top_left.y()+requested_size.height()));
+
+        draw_polygon(surface,list_frame,color,clipper);
+        */
+        /*
+        for(std::list<Widget*>::iterator it = children.begin();it!= children.end();it++){
+            (*it)->draw(surface,pick_surface,clipper);
+        }
+        */
         return;
     }
 
