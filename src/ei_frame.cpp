225d445bc15fdfa06ae4d699d7639bc5aa123e41
Alexis Perignon <aperigno@emi.u-bordeaux1.fr>
2019-04-01 12:44:33 +0200

    ei_frame commenté , raccourci , macro défini

--- src/ei_frame.cpp
+++ src/ei_frame.cpp
@@ -4,26 +4,35 @@
 #include "ei_geometrymanager.h"
 #include "hw_interface.h"
 #include <iostream>
-
+#define ALPHA_MAX 255
 
 namespace ei {
-
-    Frame::Frame(Widget *parent):Widget("Frame",parent){
-        relief=ei_relief_none;
-        text=nullptr;
-        text_font=hw_text_font_create(default_font_filename, font_default_size);
-        text_color=font_default_color;
-        text_anchor=ei_anc_center;
-        img=nullptr;
-        img_rect=nullptr;
-        img_anchor = ei_anc_center;
-        addTag("Frame");
-    }
-
-    Frame::~Frame(){
-        hw_text_font_free(text_font);
-    }
-    /**
+/**
+     * \brief   Frame constructor.
+     * @param   Parent of the frame widget
+**/
+Frame::Frame(Widget *parent):Widget("Frame",parent){
+    //Initialiaze defaults value
+    relief=ei_relief_none;
+    //Initialiaze defaults text value and text is nullptr by default (no text)
+    text=nullptr;
+    text_font=hw_text_font_create(default_font_filename, font_default_size);
+    text_color=font_default_color;
+    text_anchor=ei_anc_center;
+    //Initialiaze defaults img value and img is nullptr by default (no img)
+    img=nullptr;
+    img_rect=nullptr;
+    img_anchor = ei_anc_center;
+    //Tag for later used if event called with tag Frame
+    addTag("Frame");
+}
+/**
+     * \brief   Frame deconstructor.
+**/
+Frame::~Frame(){
+    hw_text_font_free(text_font);
+}
+/**
      * \brief   Method that draws the widget.
      *
      * @param   surface     Where to draw the widget. The actual location of the widget in the
@@ -32,79 +41,77 @@ namespace ei {
      * @param   clipper     If not NULL, the drawing is restricted within this rectangle
      *                      (expressed in the surface reference frame).
      */
-    void Frame::draw(surface_t surface,
-                    surface_t pick_surface,
-                    Rect *clipper)
-    {
-
-        
-        if(!surface){
-          fprintf(stderr,"Error occured for Frame::draw - surface is not valid\n");
-          exit(EXIT_FAILURE);
-        }
-
-        if(!pick_surface){
-          fprintf(stderr,"Error occured for Frame::draw - pick_surface is not vaild\n");
-          exit(EXIT_FAILURE);
-        }
-        linked_point_t list_frame;
-        list_frame.push_back(screen_location.top_left);
-        list_frame.push_back(Point(screen_location.top_left.x()+requested_size.width(),
-                                   screen_location.top_left.y()));
-        list_frame.push_back(Point(screen_location.top_left.x()+requested_size.width(),
-                                   screen_location.top_left.y()+requested_size.height()));
-        list_frame.push_back(Point(screen_location.top_left.x(),
-                                   screen_location.top_left.y()+requested_size.height()));
-        
-        hw_surface_lock(pick_surface);
-        pick_color.alpha=255;
-        draw_polygon(pick_surface,list_frame,pick_color,clipper);
-        hw_surface_unlock(pick_surface);
-        draw_polygon(surface,list_frame,color,clipper);
-        if (text)
-        {
-            Point where = anchor_to_pos(screen_location, text_anchor);
-            draw_text(surface, &where, text, text_font, &text_color);
-        }
-        if(img){
-            cout<<img<<endl;
+void Frame::draw(surface_t surface,
+                 surface_t pick_surface,
+                 Rect *clipper)
+{
 
 
-            if(img_rect){ //case where only subpart of img should be display.
-                hw_surface_lock(img);
-                hw_surface_lock(surface);
-                for (int i =img_rect->top_left.x();i<img_rect->top_left.x()+img_rect->size.width();i++) {
-                    for (int j = img_rect->top_left.y();j<img_rect->top_left.y()+img_rect->size.height();j++) {
-                        Point pos = Point(i,j);
-                        color_t img_c = hw_get_pixel(img,pos);
-                        hw_put_pixel(surface,pos,img_c);
-                    }
-                }
-                hw_surface_unlock(surface);
-                hw_surface_unlock(img);
-
-            }else {       //default case, display img according to img_anchor.
-                cout<<"img detected"<<endl;
-                Point pos = anchor_to_pos(screen_location,img_anchor);
-                ei_copy_surface(surface,img,&pos,EI_TRUE);
-            }
-            
-
-
-        }
-        //recursive method that draw all the children.
-        if(!children.empty()){
-            for (std::list<Widget *>::iterator it = children.begin(); it != children.end(); it++)
-            {
-                //Children should be display inside the content_rect of his parent.
-                (*it)->draw(surface, pick_surface, content_rect);
-            }
-        }
-        
-
+    if(!surface){
+        fprintf(stderr,"Error occured for Frame::draw - surface is not valid\n");
+        exit(EXIT_FAILURE);
     }
 
-    /**
+    if(!pick_surface){
+        fprintf(stderr,"Error occured for Frame::draw - pick_surface is not vaild\n");
+        exit(EXIT_FAILURE);
+    }
+    //Initialize frame position in list_frame
+    linked_point_t list_frame;
+    list_frame.push_back(screen_location.top_left);
+    list_frame.push_back(Point(screen_location.top_left.x()+requested_size.width(),
+                               screen_location.top_left.y()));
+    list_frame.push_back(Point(screen_location.top_left.x()+requested_size.width(),
+                               screen_location.top_left.y()+requested_size.height()));
+    list_frame.push_back(Point(screen_location.top_left.x(),
+                               screen_location.top_left.y()+requested_size.height()));
+    //Draw on pick_surface the rectangle  with frame's pick_color.
+    hw_surface_lock(pick_surface);
+    pick_color.alpha=ALPHA_MAX;
+    draw_polygon(pick_surface,list_frame,pick_color,clipper);
+    hw_surface_unlock(pick_surface);
+    //Finally draw the frame on the main surface
+    draw_polygon(surface,list_frame,color,clipper);
+    if (text)
+    {
+        Point where = anchor_to_pos(screen_location, text_anchor);
+        draw_text(surface, &where, text, text_font, &text_color);
+    }
+    if(img){
+        //Case where only subpart of img should be display.
+        if(img_rect){
+            hw_surface_lock(img);
+            hw_surface_lock(surface);
+            for (int i =img_rect->top_left.x(); i < img_rect->top_left.x() + img_rect->size.width();i++) {
+                for (int j = img_rect->top_left.y(); j < img_rect->top_left.y() + img_rect->size.height();j++) {
+                    Point pos = Point(i,j);
+                    color_t img_c = hw_get_pixel(img,pos);
+                    hw_put_pixel(surface,pos,img_c);
+                }
+            }
+            hw_surface_unlock(surface);
+            hw_surface_unlock(img);
+        //Default case, display img according to img_anchor.
+        }else {
+            cout<<"img detected"<<endl;
+            Point pos = anchor_to_pos(screen_location,img_anchor);
+            ei_copy_surface(surface,img,&pos,EI_TRUE);
+        }
+
+
+
+    }
+    //recursive method that draw all the children.
+    if(!children.empty()){
+        for (std::list<Widget *>::iterator it = children.begin(); it != children.end(); it++)
+            //Children should be display inside the content_rect of his parent.
+            (*it)->draw(surface, pick_surface, content_rect);
+    }
+
+
+}
+
+/**
      * @brief   Configures the attributes of widgets of the class "frame".
      *
      *      Parameters obey the "default" protocol: if a parameter is "NULL" and it has never
@@ -142,97 +149,101 @@ namespace ei {
      *                      Defaults to \ref ei_anc_center.
      */
 
-    void Frame::configure (Size*           requested_size,
-                           const color_t*  color,
-                           int*            border_width,
-                           relief_t*       relief,
-                           const char**          text,
-                           font_t*         text_font,
-                           color_t*        text_color,
-                           anchor_t*       text_anchor,
-                           surface_t*      img,
-                           Rect**          img_rect,
-                           anchor_t*       img_anchor){
-        if(img && text) fprintf(stderr,"Only one of the parameter \"text\" and \"img\" should be used (i.e. non-NULL).");
-        Widget::configure(requested_size,color);
-        if(relief) this->relief = *relief;
-        if(text && !img) this->text = *text;
-        if(text_font) this->text_font = *text_font;
-        if(text_color) this->text_color = *text_color;
-        if(img && !text) {this->img = *img;}
-        //if(img) {this->img = hw_image_load(DATA_DIR"img.jpg");}
-        if(img_rect) this->img_rect = *img_rect;
-        if(img_anchor)this->img_anchor = *img_anchor;
-        if(geom_manager)geom_manager->run(this);
-    }
+void Frame::configure (Size*           requested_size,
+                       const color_t*  color,
+                       int*            border_width,
+                       relief_t*       relief,
+                       const char**          text,
+                       font_t*         text_font,
+                       color_t*        text_color,
+                       anchor_t*       text_anchor,
+                       surface_t*      img,
+                       Rect**          img_rect,
+                       anchor_t*       img_anchor){
+    if(img && text) fprintf(stderr,"Only one of the parameter \"text\" and \"img\" should be used (i.e. non-NULL).");
+    //Call the configure function of herited class widget
+    Widget::configure(requested_size,color);
+    //Assign values and run the geometry manager
+    if(relief) this->relief = *relief;
+    if(text && !img) this->text = *text;
+    if(text_font) this->text_font = *text_font;
+    if(text_color) this->text_color = *text_color;
+    if(img && !text) this->img = *img;
+    if(img_rect) this->img_rect = *img_rect;
+    if(img_anchor) this->img_anchor = *img_anchor;
+    if(geom_manager) geom_manager->run(this);
+}
+/**
+ * @brief Frame::to_string ( string with all informations you need for debugging)
+ * @return the stream string
+ */
+string Frame::to_string()
+{
+    stringstream stream;
+    stream << Widget::to_string();
+    stream << "relief_t relief : " << relief << "\n";
+    stream << "char** text : " << text << "\n";
+    stream << "font_t text_font : " << text_font << "\n";
+    stream << "color_t text_color : "
+           << "Red : " << (int)text_color.red << " Green : " << (int)text_color.green << " Blue : " << (int)text_color.blue << " Alpha : " << (int)text_color.alpha << "\n";
+    stream << "anchor_t text_anchor : " << text_anchor << "\n";
+    stream << "surface_t* img : " << img << "\n";
+    if (img_rect)
+        stream << "Rect* img_rect : "
+               << "Width : " << img_rect->size.width() << " Height : " << img_rect->size.height() << " X : " << img_rect->top_left.x() << " Y : " << img_rect->top_left.y() << "\n";
+    stream << "anchor_t* img_anchor : " << img_anchor << "\n";
+    return stream.str();
+}
+//GETTER & SETTER
 
-    string Frame::to_string()
-    {
-        stringstream stream;
-        stream << Widget::to_string();
-        stream << "relief_t relief : " << relief << "\n";
-        stream << "char** text : " << text << "\n";
-        stream << "font_t text_font : " << text_font << "\n";
-        stream << "color_t text_color : "
-               << "Red : " << (int)text_color.red << " Green : " << (int)text_color.green << " Blue : " << (int)text_color.blue << " Alpha : " << (int)text_color.alpha << "\n";
-        stream << "anchor_t text_anchor : " << text_anchor << "\n";
-        stream << "surface_t* img : " << img << "\n";
-        if (img_rect)
-            stream << "Rect* img_rect : "
-                   << "Width : " << img_rect->size.width() << " Height : " << img_rect->size.height() << " X : " << img_rect->top_left.x() << " Y : " << img_rect->top_left.y() << "\n";
-        stream << "anchor_t* img_anchor : " << img_anchor << "\n";
-        return stream.str();
-    }
-        //GETTER & SETTER
-        
-        relief_t Frame::get_relief(){
-            return this->relief;
-        }
-        void Frame::set_relief(relief_t relief){
-            this->relief = relief;
-        }
-        const char * Frame::get_text(){
-            return this->text;
-        }
-        void Frame::set_text(const char * text){
-            this->text = text;
-        }
-        font_t Frame::get_text_font(){
-            return this->text_font;
-        }
-        void Frame::set_text_font(font_t text_font){
-            this->text_font = text_font;
-        }
-        color_t Frame::get_text_color(){
-            return this->text_color;
-        }
-        void Frame::set_text_color(color_t text_color){
-            this->text_color = text_color;
-        }
-        anchor_t Frame::get_text_anchor(){
-            return this->text_anchor;
-        }
-        void Frame::set_text_anchor(anchor_t text_anchor){
-            this->text_anchor=text_anchor;
-        }
-        surface_t Frame::get_img(){
-            return this->img;
-        }
-        void Frame::get_img(surface_t img){
-            this->img=img;
-        }
-        Rect * Frame::get_img_rect(){
-            return this->img_rect;
-        }
-        void Frame::set_img_rect(Rect * img_rect){
-            this->img_rect = img_rect;
-        
-        }
-        anchor_t Frame::get_img_anchor(){
-            return this->img_anchor;
-        }
-        void Frame::set_img_anchor(anchor_t img_anchor){
-            this->img_anchor=img_anchor;
-        }
-        //END GETTER & SETTER
+relief_t Frame::get_relief(){
+    return this->relief;
+}
+void Frame::set_relief(relief_t relief){
+    this->relief = relief;
+}
+const char * Frame::get_text(){
+    return this->text;
+}
+void Frame::set_text(const char * text){
+    this->text = text;
+}
+font_t Frame::get_text_font(){
+    return this->text_font;
+}
+void Frame::set_text_font(font_t text_font){
+    this->text_font = text_font;
+}
+color_t Frame::get_text_color(){
+    return this->text_color;
+}
+void Frame::set_text_color(color_t text_color){
+    this->text_color = text_color;
+}
+anchor_t Frame::get_text_anchor(){
+    return this->text_anchor;
+}
+void Frame::set_text_anchor(anchor_t text_anchor){
+    this->text_anchor=text_anchor;
+}
+surface_t Frame::get_img(){
+    return this->img;
+}
+void Frame::get_img(surface_t img){
+    this->img=img;
+}
+Rect * Frame::get_img_rect(){
+    return this->img_rect;
+}
+void Frame::set_img_rect(Rect * img_rect){
+    this->img_rect = img_rect;
+
+}
+anchor_t Frame::get_img_anchor(){
+    return this->img_anchor;
+}
+void Frame::set_img_anchor(anchor_t img_anchor){
+    this->img_anchor=img_anchor;
+}
+//END GETTER & SETTER
 }
