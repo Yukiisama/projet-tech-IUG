c3bbde80151c5ab75be9b1cd8b74c8cb0fe6b0cd
Alexis Perignon <aperigno@emi.u-bordeaux1.fr>
2019-04-01 12:32:57 +0200

    ei_eventmanager commenté , raccourci

--- src/ei_eventmanager.cpp
+++ src/ei_eventmanager.cpp
@@ -9,6 +9,10 @@ namespace ei
 EventManager::EventManager() {}
 /**
      * \brief	Binds a callback to an event type and a widget or a tag.
+     * Bind tags :
+     *  if tag = all, then push root and all it's children to hashmap[eventtype]
+     *  if tag != all then checkthe tag_list of all the widget with the param tag
+     *  if tag belong to tag_list then bind that widget to hashmap.
      *
      * @param	eventtype	The type of the event.
      * @param	widget		The callback is only called if the event is related to this widget.
@@ -19,15 +23,7 @@ EventManager::EventManager() {}
      * @param	callback	The callback (i.e. the function to call).
      * @param	user_param	A user parameter that will be passed to the callback when it is called.
      */
-/*
- *
- * Bind tags :
- * if tag = all, then push root and all it's children to hashmap[eventtype]
- * if tag != all then checkthe tag_list of all the widget with the param tag
- *  if tag belong to tag_list then bind that widget to hashmap.
- *
- *
- */
+
 void EventManager::bind(ei_eventtype_t eventtype,
                         Widget *widget,
                         tag_t tag,
@@ -40,9 +36,10 @@ void EventManager::bind(ei_eventtype_t eventtype,
         fprintf(stderr, "Please check widget and tag\n");
         return;
     }
-    param_callback _callback; //creat param_callback structure to bind.
+    //create param_callback structure to bind.
+    param_callback _callback;
 
-    //Bind with widget
+    //Bind with widget (tag must be empty)
     if (widget && tag.empty())
     {
         _callback.widget = widget;
@@ -50,26 +47,34 @@ void EventManager::bind(ei_eventtype_t eventtype,
         _callback.user_param = user_param;
         //Add eventtype as key, if it's not in hashMap.
         if (hashMap.find(eventtype) == hashMap.end()) hashMap[eventtype] = std::vector<param_callback>();
-        hashMap[eventtype].push_back(_callback); //add _callback to the vecot where it's key is eventtype.
+        //Add _callback to the vec where it's key is eventtype
+        hashMap[eventtype].push_back(_callback);
 
     }
 
-    //Bind with tag
+    //Bind with tag (widget must be NULL)
     else if (!widget && !tag.empty())
     {
-       std::stack<Widget *> w_stack;//use to store all widgets
-       w_stack.push(Application::getInstance()->root_widget()); //push root to the head of the stack
-       while (!w_stack.empty()) {
-           Widget* widget1=w_stack.top();//store the head of the stack to widget
-           w_stack.pop();//remove the head of the stack
-           if(!tag.compare("all")){//case all
-               //Add eventtype as key, if it's not in hashMap.
-               if(hashMap.find(eventtype)==hashMap.end()) hashMap[eventtype]=std::vector<param_callback>();
-               _callback.widget=widget1;
-               _callback.callback = callback;
-               _callback.user_param = user_param;
-               hashMap[eventtype].push_back(_callback);
-           }else{//Bind the tag that is type of class(Frame,Button and Toplevel) or custom tags.
+        //Use to store all widgets
+        std::stack<Widget *> w_stack;
+        //Push root to the head of the stack
+        w_stack.push(Application::getInstance()->root_widget());
+        while (!w_stack.empty()) {
+            //Store the head of the stack to widget
+            Widget* widget1=w_stack.top();
+            //Remove the head of the stack
+            w_stack.pop();
+            //Case all
+            if(!tag.compare("all")){
+                //Add eventtype as key, if it's not in hashMap.
+                if(hashMap.find(eventtype)==hashMap.end()) hashMap[eventtype]=std::vector<param_callback>();
+                _callback.widget=widget1;
+                _callback.callback = callback;
+                _callback.user_param = user_param;
+                 //Add _callback to the vec where it's key is eventtype
+                hashMap[eventtype].push_back(_callback);
+            }else{
+                //Bind the tag that is type of class(Frame,Button and Toplevel) or custom tags.
                 std::list<tag_t>tag_list = widget1->getTag_list();
                 for(std::list<tag_t>::iterator it = tag_list.begin(); it!=tag_list.end();++it){
                     if(!tag.compare(*it)){
@@ -77,17 +82,18 @@ void EventManager::bind(ei_eventtype_t eventtype,
                         _callback.widget=widget1;
                         _callback.callback = callback;
                         _callback.user_param = user_param;
+                         //Add _callback to the vec where it's key is eventtype
                         hashMap[eventtype].push_back(_callback);
                     }
                 }
-           }
-           //add children to stack
-           list<Widget *> w_child = widget1->getChildren();
-           for (list<Widget *>::iterator it = w_child.begin();it!=w_child.end();++it){
-               w_stack.push(*it);
-           }
+            }
+            //Add children to stack
+            list<Widget *> w_child = widget1->getChildren();
+            for (list<Widget *>::iterator it = w_child.begin();it!=w_child.end();++it)
+                w_stack.push(*it);
 
-       }
+
+        }
 
     }
 
@@ -95,18 +101,16 @@ void EventManager::bind(ei_eventtype_t eventtype,
 
 /**
      * \brief	Unbinds a callback from an event type and widget or tag.
-     *
+     * Unbind tag :
+     *   if tag = all then unbind the whole hashmap
+     *   else compare tag with tag_list of the the widget in the hashmap and compare other params,
+     *   if equal then unbind
      * @param	eventtype, widget, tag, callback, user_param
      *			All parameters must have the same value as when
      *          \ref ei::EventManager::bind was called to create the binding.
      */
 
-/*
- * Unbind tag :
- * if tag = all then unbind the whole hashmap
- * else compare tag with tag_list of the the widget in the hashmap and compare other params,
- *       if equal then unbind
- */
+
 void EventManager::unbind(ei_eventtype_t eventtype,
                           Widget *widget,
                           tag_t tag,
@@ -114,26 +118,29 @@ void EventManager::unbind(ei_eventtype_t eventtype,
                           void *user_param)
 {
 
-    //unbind with tags
+    //Unbind with tags
     if (!widget && !tag.empty())
     {
-        if (hashMap.find(eventtype) != hashMap.end())//unbind only if eventtype is in the hashmap
+        //Unbind only if eventtype is in the hashmap
+        if (hashMap.find(eventtype) != hashMap.end())
         {
-            //case tag equals to all, then unbind all widget for this particulier eventtype
+            //Case tag equals to all, then unbind all widget for this particulier eventtype
             if(!tag.compare("all")){
                 for (std::vector<param_callback>::iterator it = hashMap[eventtype].begin(); it != hashMap[eventtype].end();)
                 {
                     if ( it->user_param == user_param
-                    && it->callback.target<bool_t(Widget *, Event *, void *)>() == callback.target<bool_t(Widget *, Event *, void *)>())
+                         && it->callback.target<bool_t(Widget *, Event *, void *)>() == callback.target<bool_t(Widget *, Event *, void *)>())
                         it = hashMap[eventtype].erase(it);
                     else ++it;
                 }
-            }else{//unbind the param_callback that has the parameters as in unbind fonction.
+            }else{
+                //Unbind the param_callback that has the parameters as in unbind fonction.
                 for (std::vector<param_callback>::iterator it = hashMap[eventtype].begin(); it != hashMap[eventtype].end();)
                 {
                     if ( it->user_param == user_param
-                    && it->callback.target<bool_t(Widget *, Event *, void *)>() == callback.target<bool_t(Widget *, Event *, void *)>())
+                         && it->callback.target<bool_t(Widget *, Event *, void *)>() == callback.target<bool_t(Widget *, Event *, void *)>())
                     {
+                        //Get tag_list and delete tag that differs with it_tag
                         linked_tag_t tag_list = it->widget->getTag_list();
                         for(linked_tag_t::iterator it_tag = tag_list.begin(); it_tag!=tag_list.end();++it_tag){
                             if(!tag.compare(*it_tag))
@@ -154,28 +161,33 @@ void EventManager::unbind(ei_eventtype_t eventtype,
     {
         for (std::vector<param_callback>::iterator it = hashMap[eventtype].begin(); it != hashMap[eventtype].end();++it)
         {
-            if (it->widget->getPick_id() == widget->getPick_id() && it->user_param == user_param 
-            && it->callback.target<bool_t(Widget *, Event *, void *)>() == callback.target<bool_t(Widget *, Event *, void *)>())
+            if (it->widget->getPick_id() == widget->getPick_id() && it->user_param == user_param
+                    && it->callback.target<bool_t(Widget *, Event *, void *)>() == callback.target<bool_t(Widget *, Event *, void *)>())
                 it = hashMap[eventtype].erase(it);
         }
     }
 }
-
+/**
+     * \brief	Handle the events and add invalid rects into the singleton Application
+     * @param   event the event that we need to handle
+     *
+**/
 void EventManager::eventHandler(Event *event)
 {
-
+    //Iterate and callback the associated function to the event
     for (std::vector<param_callback>::iterator it = hashMap[event->type].begin(); it != hashMap[event->type].end(); ++it)
-           {
-               if (it->widget)
-               {
-                   //Application::getInstance()->invalidate_rect((*it->widget->getContent_rect()));
-                   if(it->callback(it->widget, event, it->user_param)==EI_TRUE){
-                       Application::getInstance()->invalidate_rect((*it->widget->getContent_rect()));
-                       break;
-                   }
-                   if(!it->widget->getName().compare("Button")) Application::getInstance()->invalidate_rect((*it->widget->getContent_rect()));
-               }
-           }
+    {
+        if (it->widget)
+        {
+            //Application::getInstance()->invalidate_rect((*it->widget->getContent_rect()));
+            if(it->callback(it->widget, event, it->user_param)){
+                Application::getInstance()->invalidate_rect((*it->widget->getContent_rect()));
+                break;
+            }
+            if(!it->widget->getName().compare("Button"))
+                Application::getInstance()->invalidate_rect((*it->widget->getContent_rect()));
+        }
+    }
 
 }
 
