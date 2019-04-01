61020a6bdef1335ef708be79b31f03e76c71e676
Alexis Perignon <aperigno@emi.u-bordeaux1.fr>
2019-04-01 10:53:00 +0200

    ei_application commenté et raccourci

--- src/ei_application.cpp
+++ src/ei_application.cpp
@@ -20,14 +20,17 @@ namespace ei {
      * @param main_window_size  the size of the root window of the application.
      */
     Application::Application(Size* main_window_size){
-      if(main_window_size == nullptr){
+      if(!main_window_size){
         fprintf(stderr,"Error occured for Application::Application - main_window_size is nullptr\n");
         exit(EXIT_FAILURE);
       }
+      //Initialises access to the low-level operating system services.
       hw_init();
+      //create root and offscreen surface , and the root widget being a frame
       this->root_window = hw_create_window(main_window_size, EI_FALSE);
       this->offscreen = hw_surface_create(this->root_window, main_window_size);
       this->widget_root = new Frame(NULL);
+      //singleton application
       this->instance = this;
     }
 
@@ -47,11 +50,13 @@ namespace ei {
     {
         MouseEvent* e = static_cast<MouseEvent*>(event);
         Toplevel* top = static_cast<Toplevel*>(widget);
-
-        if(top->moving()==EI_TRUE || top->resizing()==EI_TRUE || top->closing()==EI_TRUE){
-            if(top->moving()==EI_TRUE)top->set_top_bar_clicked(EI_FALSE);
-            if(top->resizing()==EI_TRUE)top->set_resize_button_pressed(EI_FALSE);
-            if(top->closing()==EI_TRUE){
+        //Clicked up , update if moving , resizing or closing
+        if(top->moving()|| top->resizing()|| top->closing()){
+            if(top->moving())
+                top->set_top_bar_clicked(EI_FALSE);
+            if(top->resizing())
+                top->set_resize_button_pressed(EI_FALSE);
+            if(top->closing()){
                 if(Application::getInstance()->widget_pick(e->where)->getPick_id()==top->getButton_close()->getPick_id()){
                     delete top;
                 }
@@ -78,7 +83,7 @@ namespace ei {
         //The case when the user is clicking on the toplevel
         if(Application::getInstance()->widget_pick(e->where)->getPick_id()==top->getPick_id()){
             //The case when the user is clicking on the top_bar
-            if(top->inside_top_bar(e->where)==EI_TRUE){
+            if(top->inside_top_bar(e->where)){
                 //Tells the toplevel that its top_bar is clicked
                 top->set_top_bar_clicked(EI_TRUE);
                 top->setMouse_pos(e->where);
@@ -114,29 +119,32 @@ namespace ei {
         Toplevel* top = static_cast<Toplevel*>(widget);
 
         //The case when the top_bar is still pressed and the mouse is moving
-        if(top->moving()==EI_TRUE){
+        if(top->moving()){
             if(Application::getInstance()->inside_root(e->where)){//check if the mouse position is valid
                 double move_x = (e->where.x())-(top->getMouse_pos().x());
                 double move_y = (e->where.y())-(top->getMouse_pos().y());
-
+                //Update geom_manager x & y to update the position of toplevel
                 top->getGeom_manager()->setX(top->getScreen_location().top_left.x()+move_x);
                 top->getGeom_manager()->setY(top->getScreen_location().top_left.y()+move_y);
+                //finally run the geom_manager that will result in updating the position
                 top->getGeom_manager()->run(top);
                 top->setMouse_pos(e->where);
             }
             return EI_TRUE;
         }
         //The case when the user is clicking on the resize button and moving the mouse
-        if(top->resizing()==EI_TRUE){
+        if(top->resizing()){
             if(Application::getInstance()->inside_root(e->where)){
                 double new_width = (e->where.x())-(top->getScreen_location().top_left.x());
                 double new_height = (e->where.y())-(top->getScreen_location().top_left.y());
+                //Limit the top level to a minimal size
                 if(new_width < top->getMin_size().width()){
                     new_width = top->getMin_size().width();
                 }
                 if(new_height < top->getMin_size().height()){
                     new_height = top->getMin_size().height();
                 }
+                //finally update the new size of the top level
                 Size *new_size = new Size(new_width,new_height);
                 top->configure(new_size,NULL,NULL,NULL,NULL,NULL,NULL);
                 delete(new_size);
@@ -153,14 +161,19 @@ namespace ei {
      *    \ref app_quit_request is called.
      */
     void Application::run(){
-        //Binding the default comportments of widgets
+        //Binding the default comportments of TopLevel class of widget
         EventManager::getInstance().bind(ei_ev_mouse_buttonup, NULL, "Toplevel", toplevel_click_up, NULL);
         EventManager::getInstance().bind(ei_ev_mouse_buttondown, NULL, "Toplevel", toplevel_click_down, NULL);
         EventManager::getInstance().bind(ei_ev_mouse_move, NULL, "Toplevel", default_toplevel, NULL);
         running = true;
+        //used to limit fps of the application
         double current_time ;
+        //We first need to add the window rect to invalid rect list in order to update it at launch
         Rect window_rect = hw_surface_get_rect(root_window);
         invalidate_rect(window_rect);
+        /*This loop waits an event , then treat it with the event manager
+         *Then update the screen and limit it to 60 fps
+         */
         while(running){
             Event *ev = hw_event_wait_next();
             if( ev->type == ei_ev_keydown ){
@@ -171,7 +184,7 @@ namespace ei {
             }
             current_time = hw_now();
             if(update_time<=current_time){
-
+                //Screen need to be update , draw the widgets then update rects
                 if(!to_clear_rectangle_list.empty()){
                     widget_root->draw(root_window,offscreen,widget_root->getContent_rect());
                     hw_surface_update_rects(to_clear_rectangle_list);
@@ -193,12 +206,6 @@ namespace ei {
      *        A copy is made, so it is safe to release the rectangle on return.
      */
     void Application::invalidate_rect(const Rect &rect){
-        /*
-        if(&rect == NULL){
-          fprintf(stderr,"Error occured for Application::invalide_rect - rect is nullptr\n");
-          exit(EXIT_FAILURE);
-        }
-        */
         to_clear_rectangle_list.push_back(rect);
     }
 
@@ -208,7 +215,6 @@ namespace ei {
      */
     void Application::quit_request(){
         running = false;
-        //delete this;
     }
 
     /**
@@ -244,9 +250,25 @@ namespace ei {
           fprintf(stderr,"Error occured for Application::widget_pick - param where is out the root_window\n");
           exit(EXIT_FAILURE);
       }
+      //Get the pixel at the where pos then convert it to get the unique id of the widget being clicked
       color_t color = hw_get_pixel(this->offscreen, where);
       uint32_t ID = widget_root->conver_color_id(color);
       return widget_root->pick(ID);
+    }
+
+    /**
+     * @brief   Returns if the point where is inside the root
+     *
+     * @param   where       The location on screen, expressed in the root window coordinates.
+     *
+     * @return      True if inside_root else False
+     */
+    bool_t Application::inside_root (const Point& where){
+        if((where.x() < 0 || where.x() > hw_surface_get_size(this->root_window).width())
+        || (where.y() < 0 || where.y() > hw_surface_get_size(this->root_window).height())){
+            return EI_FALSE;
+        }
+        return EI_TRUE;
     }
       // *** Getters & Setter ***
     surface_t Application::get_root_window(){
@@ -287,12 +309,5 @@ namespace ei {
     }
     // *** End Getter & Setter ***
 
-    bool_t Application::inside_root (const Point& where){
-        if((where.x() < 0 || where.x() > hw_surface_get_size(this->root_window).width())
-        || (where.y() < 0 || where.y() > hw_surface_get_size(this->root_window).height())){
-            return EI_FALSE;
-        }
-        return EI_TRUE;
-    }
 
 }
