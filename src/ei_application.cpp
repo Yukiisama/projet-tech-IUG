ae0ef77c948b6e36875ff6bc9ae233eb5f1473ce
jdakata <jean-david.akata@etu.u-bordeaux.fr>
2019-04-06 17:53:42 +0200

    optimized rect not finished

--- src/ei_application.cpp
+++ src/ei_application.cpp
@@ -98,7 +98,6 @@ bool_t toplevel_click_down(Widget* widget, Event* event, void* user_param)
             //Tells the toplevel that its top_bar is clicked
             top->set_top_bar_clicked(EI_TRUE);
             top->setMouse_pos(e->where);
-
             return EI_TRUE;
         }
     }
@@ -126,7 +125,7 @@ bool_t toplevel_click_down(Widget* widget, Event* event, void* user_param)
      */
 bool_t default_toplevel(Widget* widget, Event* event, void* user_param)
 {
-    cout<<"top defaut called"<<endl;
+    //cout<<"top defaut called"<<endl;
     MouseEvent* e = static_cast<MouseEvent*>(event);
     Toplevel* top = static_cast<Toplevel*>(widget);
 
@@ -168,6 +167,164 @@ bool_t default_toplevel(Widget* widget, Event* event, void* user_param)
     return EI_FALSE;
 }
 
+//bool Application::isIntersect(Rect rect1, Rect rect2){
+//    //The points of rect1 in clockwise.
+//    linked_point_t rect1_points;
+//    rect1_points.push_back(rect1.top_left);
+//    rect1_points.push_back(Point(rect1.top_left.x()+rect1.size.width(),rect1.top_left.y()));
+//    rect1_points.push_back(Point(rect1.top_left.x()+rect1.size.width(),rect1.top_left.y()+rect1.size.height()));
+//    rect1_points.push_back(Point(rect1.top_left.x(),rect1.top_left.y()+rect1.size.height()));
+
+//    //Rect2 coordinates
+//    int x = rect2.top_left.x();
+//    int end_x = x + rect2.size.width();
+//    int y = rect2.top_left.y();
+//    int end_y = y + rect2.size.height();
+
+//    for(linked_point_t::iterator it = rect1_points.begin(); it!=rect1_points.end(); ++it){
+//        if(it->top_left.x()>x && it->top_left.x()<end_x && it->top_left.y()>y && it->top_left.y()<end_y)
+//            return true;
+//    }
+//    return false;
+//}
+
+bool_t Application::rectFusion(Rect* rect1, Rect* rect2){
+    int new_rect_width = 0;//The width of the fusion of rect1 and rect2
+    int new_rect_height = 0;//The height of the fusion of rect1 and rect2
+    int i_width = 0;//The width of the intersection between rect1 and rect2.
+    int i_height = 0;//The height of the intersection between rect1 and rect2.
+    int x1 = rect1->top_left.x();
+    int x2 = rect2->top_left.x();
+    int y1 = rect1->top_left.y();
+    int y2 = rect2->top_left.y();
+    int w1 = rect1->size.width();
+    int w2 = rect2->size.width();
+    int h1 = rect1->size.height();
+    int h2 = rect2->size.height();
+    int end_x1 = x1 + w1;//The position x of rect1's top_right corner.
+    int end_x2 = x2 + w2;//The position x of rect1's top_right corner.
+    int end_y1 = y1 + h1;//The position y of rect1's bottom_left corner.
+    int end_y2 = y2 + h2;//The position y of rect2's bottom_left corner.
+    int area1 = w1*h1;//The area of rect1.
+    int area2 = w2*h2;//The area of rect2.
+    int p_area1 = 0;//The percentage that the intersected part with rect2 represents on the the total area of rect1.
+    int p_area2 = 0;//The percentage that the intersected part with rect1 represents on the the total area of rect2.
+    int i_area = 0;//The intersected area between rect1 and rect2.
+
+    //The case when the top_left corner of rect1 is inside rect2
+    if(x1>=x2 && y1>=y2 && x1<end_x2 && y1<end_y2){
+        //std::cout<<"entered case 1"<<endl;
+        if(end_x1<end_x2) { i_width = w1; new_rect_width = w2; } else { i_width = end_x2-x1; new_rect_width = end_x1-x2; }
+        if(end_y1<end_y2) { i_height = h1; new_rect_height = h2; } else { i_height = end_y2-y1; new_rect_height = end_y1-y2; }
+        i_area = i_width*i_height;
+//        std::cout<<"i_area = "<<i_area<<endl;
+//        std::cout<<"area1 = "<<area1<<endl;
+//        std::cout<<"area2 = "<<area2<<endl;
+        p_area1 = (1-((area1-i_area)/area1))*100;
+        p_area2 = (1-((area2-i_area)/area2))*100;
+//        std::cout<<"p_area1 = "<<p_area1<<endl;
+//        std::cout<<"p_area2 = "<<p_area2<<endl;
+        if(p_area1>50 || p_area2>50){//they merge only if the average of their intersection percentage is greater than 50% for one of them.
+            rect1->top_left = rect2->top_left;
+            rect1->size.width() = new_rect_width;
+            rect1->size.height() = new_rect_height;
+            //std::cout<<"rect1 changed to = "<<rect1->size.width()<<","<<rect1->size.height()<<endl;
+            return EI_TRUE;
+        }
+        else{
+            return EI_FALSE;
+        }
+    }
+
+    //The case when the bottom_right corner of rect1 is inside rect2
+    if(x2>x1 && y2>y1 && x2<end_x1 && y2<end_y1){
+        //std::cout<<"entered case 2"<<endl;
+        if(end_x2<end_x1) { i_width = w2; new_rect_width = w1; } else { i_width = end_x1-x2; new_rect_width = end_x2-x1; }
+        if(end_y2<end_y1) { i_height = h2; new_rect_height = h1; } else { i_height = end_y1-y2; new_rect_height = end_y2-y1; }
+        i_area = i_width*i_height;
+        p_area1 = (1-((area1-i_area)/area1))*100;
+        p_area2 = (1-((area2-i_area)/area2))*100;
+        if(p_area1>50 || p_area2>50){
+            rect1->size.width() = new_rect_width;
+            rect1->size.height() = new_rect_height;
+
+            //std::cout<<"rect1 changed to = "<<rect1->size.width()<<","<<rect1->size.height()<<endl;
+            return EI_TRUE;
+        }
+        else{
+            return EI_FALSE;
+        }
+    }
+
+    //The case when the top_right corner of rect1 is inside rect2 and the top_left of rect1 is outside
+    if(x1<x2 && y1>y2 && y1<end_y2 && end_x1>x2){
+        //std::cout<<"entered case 3"<<endl;
+        if(end_x1<end_x2) { i_width = end_x1-x2; new_rect_width = end_x2-x1; } else { i_width = w2; new_rect_width = w1; }
+        if(end_y1<end_y2) { i_height = h1; new_rect_height = h2; } else { i_height = end_y2-y1; new_rect_height = end_y1-y2; }
+        i_area = i_width*i_height;
+        p_area1 = (1-((area1-i_area)/area1))*100;
+        p_area2 = (1-((area2-i_area)/area2))*100;
+        if(p_area1>50 || p_area2>50){
+            rect1->top_left.y() = y2;
+            rect1->size.width() = new_rect_width;
+            rect1->size.height() = new_rect_height;
+            //std::cout<<"rect1 changed to = "<<rect1->size.width()<<","<<rect1->size.height()<<endl;
+            return EI_TRUE;
+        }
+        else{
+            return EI_FALSE;
+        }
+    }
+
+    //The case when the bottom_left corner of rect1 is inside rect2 and the top_left of rect1 is outside
+    if(x1>x2 && x1<end_x2 && y1<y2 && end_y1>y2){
+        //std::cout<<"entered case 4"<<endl;
+        if(end_x1<end_x2) { i_width = w1; new_rect_width = w2; } else { i_width = end_x2-x1; new_rect_width = end_x1-x2; }
+        if(end_y1<end_y2) { i_height = end_y1-y2; new_rect_height = end_y2-y1; } else { i_height = h2; new_rect_height = h1; }
+        i_area = i_width*i_height;
+        p_area1 = (1-((area1-i_area)/area1))*100;
+        p_area2 = (1-((area2-i_area)/area2))*100;
+        if(p_area1>50 || p_area2>50){
+            rect1->top_left.x() = x2;
+            rect1->size.width() = new_rect_width;
+            rect1->size.height() = new_rect_height;
+            //std::cout<<"rect1 changed to = "<<rect1->size.width()<<","<<rect1->size.height()<<endl;
+            return EI_TRUE;
+        }
+        else{
+            return EI_FALSE;
+        }
+    }
+    std::cout<<"didnt entered any case"<<endl;
+    return EI_FALSE;
+}
+
+void Application::optimizedRect(){
+    /*to_clear_rectangle_list.clear();
+    to_clear_rectangle_list.push_back(Rect(Point(5,5),Size(100,60)));
+    to_clear_rectangle_list.push_back(Rect(Point(10,10),Size(110,65)));
+    to_clear_rectangle_list.push_back(Rect(Point(80,105),Size(45,45)));
+    to_clear_rectangle_list.push_back(Rect(Point(190,45),Size(180,90)));
+    to_clear_rectangle_list.push_back(Rect(Point(195,35),Size(120,60)));*/
+    if(to_clear_rectangle_list.size()>1){
+        std::cout<<"size is at the begining : "<<to_clear_rectangle_list.size()<<"\n\n"<<endl;
+        for(linked_rect_t::iterator it = to_clear_rectangle_list.begin(), next_it = ++to_clear_rectangle_list.begin(); next_it!=to_clear_rectangle_list.end();){
+            std::cout<<"it = "<<it->size.width()<<" next_it = "<<next_it->size.width()<<endl;
+            std::cout<<"rect1 at first = "<<it->size.width()<<","<<it->size.height()<<endl;
+            std::cout<<"rect2 at first = "<<next_it->size.width()<<","<<next_it->size.height()<<endl;
+            if(rectFusion(&(*it),&(*next_it))){
+                next_it = to_clear_rectangle_list.erase(next_it);
+                std::cout<<"rect1 changed to = "<<it->size.width()<<","<<it->size.height()<<endl;
+            }
+            else{
+                ++next_it;
+                ++it;
+            }
+        }
+        std::cout<<"\n\n"<<"size is at the end : "<<to_clear_rectangle_list.size()<<endl;
+    }
+}
+
 /**
      * \brief Runs the application: enters the main event loop. Exits when
      *    \ref app_quit_request is called.
@@ -183,7 +340,7 @@ void Application::run(){
     //We first need to add the window rect to invalid rect list in order to update it at launch
     Rect window_rect = hw_surface_get_rect(root_window);
     invalidate_rect(window_rect);
-    /*This loop waits an event , then treat it with the event manager
+    /*This loop wait for an event , then treat it with the event manager
      *Then update the screen and limit it to 60 fps
      */
     while(running){
@@ -198,8 +355,13 @@ void Application::run(){
         if(update_time<=current_time){
             //Screen need to be update , draw the widgets then update rects
             if(!to_clear_rectangle_list.empty()){
-                widget_root->draw(root_window,offscreen,widget_root->getContent_rect());
+                std::cout<<"rect nb :"<<to_clear_rectangle_list.size()<<endl;
+                //for(linked_rect_t::iterator it = to_clear_rectangle_list.begin(); it!=to_clear_rectangle_list.end();++it){
+
+                    widget_root->draw(root_window,offscreen,widget_root->getContent_rect());
                 hw_surface_update_rects(to_clear_rectangle_list);
+                //}
+                optimizedRect();
             }
             //next step is to clear the rectangle list.
             to_clear_rectangle_list.clear();
