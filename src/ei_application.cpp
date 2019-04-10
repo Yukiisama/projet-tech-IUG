f7f08629236c38d597c42fb287b3fbaed475bba6
aperigno <alexisperignon@live.fr>
2019-04-10 11:27:56 +0200

    hw_surface_update_rects INDISPENSABLE sinon en dehors du cremi ça bug et push_front (o(1)) au lieu de push back o(n)

--- src/ei_application.cpp
+++ src/ei_application.cpp
@@ -69,12 +69,14 @@ void Application::run(){
             //Screen need to be update , draw the widgets then update rects
             if(!to_clear_rectangle_list.empty()){
                 widget_root->draw(root_window,offscreen,widget_root->getContent_rect());
-                //hw_surface_update_rects(to_clear_rectangle_list);
+                //Dont delete hw_surface_update_rects , it will not work outside of cremi
+                hw_surface_update_rects(to_clear_rectangle_list);
             }
             //next step is to clear the rectangle list.
             to_clear_rectangle_list.clear();
             update_time  = hw_now();
         }
+
         delete ev;
     }
     return;
@@ -88,7 +90,7 @@ void Application::run(){
      *        A copy is made, so it is safe to release the rectangle on return.
      */
 void Application::invalidate_rect(const Rect &rect){
-    to_clear_rectangle_list.push_back(rect);
+    to_clear_rectangle_list.push_front(rect);
 }
 
 /**
