3fd2f109857c3e62e9a9338ec91ac9c06e790e40
chouyoux <alexis.flazinska@etu.u-bordeaux.fr>
2019-04-12 14:04:28 +0200

    [~] test de griddeur

--- tests/griddeur.cpp
+++ tests/griddeur.cpp
@@ -31,7 +31,7 @@ bool_t process_display(Widget* widget, Event* event, void* user_param)
 
 int ei_main(int argc, char* argv[])
 {
-    Size         screen_size(1600, 900);
+    Size         screen_size(800, 600);
     color_t      root_bgcol      = {0x52, 0x7f, 0xb4, 0xff};
 
     color_t      button_color    = {0x88, 0x88, 0x88, 0xff};
@@ -41,10 +41,9 @@ int ei_main(int argc, char* argv[])
     relief_t     relief          = ei_relief_raised;
     int          button_border_width = 3;
     const char*  button_title_ok     = "Ok mon ami";
-    const char*  button_title_cancel = "Cancel tout stp";
     const char*  button_title_cut = "Cut";
 
-    Size         window_size(400,400);
+    Size         window_size(300,400);
     const char*  window_title    = "Relatif";
     const char*  window_title_    = "Absolu";
     color_t      window_color    = {0xA0,0xA0,0xA0, 0xff};
@@ -52,7 +51,7 @@ int ei_main(int argc, char* argv[])
     bool_t       closable        = EI_TRUE;
     axis_set_t   window_resizable = ei_axis_both;
     Point        window_position(30, 10);
-    Point        window_position_(530, 10);
+    Point        window_position_(430, 10);
 
     Application* app = new Application(&screen_size);
     app->root_widget()->configure(&screen_size, &root_bgcol, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL);
@@ -63,15 +62,6 @@ int ei_main(int argc, char* argv[])
 
     Button* button_ok = new Button(toplevel);
     button_ok->configure (&button_size, &button_color, &button_border_width, NULL, &relief, &button_title_ok, NULL, &text_color, &text_anchor, NULL, NULL, NULL);
-
-    Button* button_ok2 = new Button(toplevel);
-    button_ok2->configure (&button_size, &button_color, &button_border_width, NULL, &relief, &button_title_ok, NULL, &text_color, &text_anchor, NULL, NULL, NULL);
-
-    Button* button_cancel = new Button(toplevel);
-    button_cancel->configure (&button_size, &button_color, &button_border_width, NULL, &relief, &button_title_cancel, NULL, &text_color, &text_anchor, NULL, NULL, NULL);
-
-    Button* button_cancel2 = new Button(toplevel);
-    button_cancel2->configure (&button_size, &button_color, &button_border_width, NULL, &relief, &button_title_cancel, NULL, &text_color, &text_anchor, NULL, NULL, NULL);
 
     Button* button_cut = new Button(toplevel);
     button_cut->configure (&button_size, &button_color, &button_border_width, NULL, &relief, &button_title_cut, NULL, &text_color, &text_anchor, NULL, NULL, NULL);
@@ -87,13 +77,9 @@ int ei_main(int argc, char* argv[])
     Griddeur* g1 = new Griddeur(toplevel, new int(18), new int(18), EI_TRUE);
     g1->addWidget(button_cut, new int(0), new int(0), new int(1), new int(1));
     g1->addWidget(button_ok, new int(1), new int(1), new int(3), new int(1));
-    g1->addWidget(button_cancel, new int(1), new int(2), new int(3), new int(1));
-    g1->addWidget(button_cancel2, new int(1), new int(3), new int(3), new int(1));
-    g1->addWidget(button_ok2, new int(4), new int(0), new int(1), new int(3));
     g1->addWidget(button_cut2, new int(0), new int(4), new int(3), new int(1));
     g1->addWidget(button_cut3, new int(4), new int(4), new int(1), new int(1));
     // redef
-    g1->addWidget(button_cancel2, new int(0), new int(0), new int(1), new int(1));
     g1->addWidget(button_cut, new int(1), new int(3), new int(3), new int(1));
     g1->run_all();
 
@@ -107,15 +93,6 @@ int ei_main(int argc, char* argv[])
     Button* button_ok2_ = new Button(toplevel_);
     button_ok2_->configure (&button_size, &button_color, &button_border_width, NULL, &relief, &button_title_ok, NULL, &text_color, &text_anchor, NULL, NULL, NULL);
 
-    Button* button_cancel_ = new Button(toplevel_);
-    button_cancel_->configure (&button_size, &button_color, &button_border_width, NULL, &relief, &button_title_cancel, NULL, &text_color, &text_anchor, NULL, NULL, NULL);
-
-    Button* button_cancel2_ = new Button(toplevel_);
-    button_cancel2_->configure (&button_size, &button_color, &button_border_width, NULL, &relief, &button_title_cancel, NULL, &text_color, &text_anchor, NULL, NULL, NULL);
-
-    Button* button_cut_ = new Button(toplevel_);
-    button_cut_->configure (&button_size, &button_color, &button_border_width, NULL, &relief, &button_title_cut, NULL, &text_color, &text_anchor, NULL, NULL, NULL);
-
     Button* button_cut2_ = new Button(toplevel_);
     button_cut2_->configure (&button_size, &button_color, &button_border_width, NULL, &relief, &button_title_cut, NULL, &text_color, &text_anchor, NULL, NULL, NULL);
 
@@ -125,16 +102,10 @@ int ei_main(int argc, char* argv[])
     Placer* p1_ = new Placer();
     p1_->configure(toplevel_, NULL, &(window_position_.x()), &(window_position_.y()), NULL, NULL, NULL, NULL, NULL, NULL);
     Griddeur* g1_ = new Griddeur(toplevel_, new int(50), new int(50), EI_FALSE);
-    g1_->addWidget(button_cut_, new int(0), new int(0), new int(1), new int(1));
     g1_->addWidget(button_ok_, new int(1), new int(1), new int(3), new int(1));
-    g1_->addWidget(button_cancel_, new int(1), new int(2), new int(3), new int(1));
-    g1_->addWidget(button_cancel2_, new int(1), new int(3), new int(3), new int(1));
     g1_->addWidget(button_ok2_, new int(4), new int(0), new int(1), new int(3));
     g1_->addWidget(button_cut2_, new int(0), new int(4), new int(3), new int(1));
     g1_->addWidget(button_cut3_, new int(4), new int(4), new int(1), new int(1));
-    // redef
-    g1_->addWidget(button_cancel2_, new int(0), new int(0), new int(1), new int(1));
-    g1_->addWidget(button_cut_, new int(1), new int(3), new int(3), new int(1));
     g1_->run_all();
 
     EventManager::getInstance().bind(ei_ev_keydown, NULL, "all", process_key, app);
